1cd21127b54a5f3c655afa1eb90d9247f95afd54
vibecode-mike <vibecode-mike@users.noreply.github.com>
2026-06-23 14:45:15 +0800

    chore(deadcode): 移除 imports/ 141 個 Figma 零引用死碼（具名元件 51 + svg 圖示 90）

--- frontend/src/imports/CardImage.tsx
+++
@@ -1,7 +0,0 @@
-export default function CardImage() {
-  return (
-    <div className="bg-[#edf0f8] content-stretch flex items-center justify-center relative size-full" data-name="Card Image">
-      <p className="font-['Noto_Sans_TC:Regular',_sans-serif] font-normal leading-[1.5] relative shrink-0 text-[#383838] text-[32px] text-center text-nowrap whitespace-pre">選擇圖片</p>
-    </div>
-  );
-}
